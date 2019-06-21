65f8c0bd4efdcd927756285cfe9a8603988e3eff
SATOTAKUYA <stakuya6@gmail.com>
2019-06-21 20:14:43 +0900

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require turbolinks
 //= require_tree .
 
-
+/*
 jQuery(function ($) {
   $('.tabcontent > div').hide();
 
@@ -29,4 +29,23 @@ jQuery(function ($) {
 
     return false;
   }).filter(':eq(0)').click();
+});
+*/
+$(function () {
+  // ①タブをクリックしたら発動
+  $('.tab li').click(function () {
+
+    // ②クリックされたタブの順番を変数に格納
+    var index = $('.tab li').index(this);
+
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab li').removeClass('active');
+
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active');
+
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.area ul').removeClass('show').eq(index).addClass('show');
+
+  });
 });

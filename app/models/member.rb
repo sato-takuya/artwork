cc4604c13fc8f3106c357644e5d855db1755b7a2
SATOTAKUYA <stakuya6@gmail.com>
2019-06-21 15:50:03 +0900

    about メンバー一覧

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,8 @@ class Member < ActiveHash::Base
   include ActiveHash::Associations
   belongs_to :group
   self.data = [
-    {:id => 1, :member_name =>"佐藤拓弥",:part => "ギター",:member_image => "icon.png",:group_id => 1},
-    {:id => 2, :member_name =>"メンバーB",:part => "ベース",:member_image => "icon.png",:group_id => 1}
+    {:id => 1, :member_name =>"メンバーの名前A",:part => "Gitar",:member_info=>"自己紹介",:member_image => "icon.png",:group_id => 1},
+    {:id => 2, :member_name =>"メンバーの名前B",:part => "Bass",:member_info=>"自己紹介",:member_image => "icon.png",:group_id => 1},
+    {:id => 3, :member_name =>"メンバーの名前C",:part => "drum",:member_info=>"自己紹介",:member_image => "icon.png",:group_id => 1}
   ]
 end

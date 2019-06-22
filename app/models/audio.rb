3b6492d4338539d4d2976afac48cbf264a021cb1
SATOTAKUYA <stakuya6@gmail.com>
2019-06-22 13:14:07 +0900

    オーディオ一応実装

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -2,10 +2,10 @@ class Audio < ActiveHash::Base
   include ActiveHash::Associations
   belongs_to :group
   self.data = [
-    {:id => 1, :source => "sound1.mp3",:title => '楽曲タイトルA', :audio_image => "audio.png",:lyrics => "私が私を語るほどに・・・",:group_id => 1},
-    {:id => 2, :source => "sound2.mp3",:title => '楽曲タイトルB', :audio_image => "audio.png",:lyrics => "冷えたコンクリート",:group_id => 1},
-    {:id => 3, :source => "sound3.mp3",:title => '楽曲タイトルC', :audio_image => "audio.png",:lyrics => "明日には大人になる君へ・・・",:group_id => 1},
-    {:id => 4, :source => "sound4.mp3",:title => '楽曲タイトルD', :audio_image => "audio.png",:lyrics => "君はまだ覚えてるかな・・・",:group_id => 1},
-    {:id => 5, :source => "sound5.mp3",:title => '楽曲タイトルE', :audio_image => "audio.png",:lyrics => "木造アパートの一階で・・・",:group_id => 1}
+    {:id => 1, :source => "glowing.mp3",:title => '楽曲タイトルA', :audio_image => "audio.png",:lyrics => "私が私を語るほどに・・・",:group_id => 1},
+    {:id => 2, :source => "glowing.mp3",:title => '楽曲タイトルB', :audio_image => "audio.png",:lyrics => "冷えたコンクリート",:group_id => 1},
+    {:id => 3, :source => "glowing.mp3",:title => '楽曲タイトルC', :audio_image => "audio.png",:lyrics => "明日には大人になる君へ・・・",:group_id => 1},
+    {:id => 4, :source => "glowing.mp3",:title => '楽曲タイトルD', :audio_image => "audio.png",:lyrics => "君はまだ覚えてるかな・・・",:group_id => 1},
+    {:id => 5, :source => "glowing.mp3",:title => '楽曲タイトルE', :audio_image => "audio.png",:lyrics => "木造アパートの一階で・・・",:group_id => 1}
   ]
 end

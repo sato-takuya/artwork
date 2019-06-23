d3279d57722f8a272d6a58148bd8106d36f490a3
SATOTAKUYA <stakuya6@gmail.com>
2019-06-23 15:08:29 +0900

    test

--- app/models/a_audio.rb
+++ app/models/a_audio.rb
@@ -9,4 +9,3 @@ class Audio < ActiveHash::Base
     {:id => 5, :source => "glowing.mp3",:title => '楽曲タイトルE', :audio_image => "audio.png",:lyrics => "木造アパートの一階で・・・",:group_id => 1,:public_uid=>'yomkfmch'}
   ]
 end
-audio = Audio.new(source: "glowing.mp3",audio_title: "楽曲タイトルA",audio_image: "audio.png",audio_lyrics: "歌詞・・・",user_id: 1,audio_public_uid: "fiskmwai")

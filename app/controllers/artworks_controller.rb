2f0ed5bbdc003a2d06604f344b1f6ee19eda22cb
SATOTAKUYA <stakuya6@gmail.com>
2019-06-24 14:59:44 +0900

    モーダルウィンドウに戻しました

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,5 @@
+class ArtworksController < ApplicationController
+  def show
+    #@content = Audio.find_by(audio_public_uid: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'trends#index'
   get '/:id' ,to: 'groups#index', param: :nickname
+  #resources :artworks
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end

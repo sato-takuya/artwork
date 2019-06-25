260aebb7ffb3c15a9587d4fcd48b3576cbebc47f
SATOTAKUYA <stakuya6@gmail.com>
2019-06-25 16:58:05 +0900

    Test

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -14,6 +14,8 @@ class AudiosController < ApplicationController
   def create
     @audio = Audio.new(audios_params)
     @audio.user_id = current_user.id
+    @audio.source = "glowing.mp3"
+    @audio.audio_image = 'noimage.png'
     if @audio.save
       redirect_to "/",notice: "保存しました"
     else
@@ -28,6 +30,8 @@ class AudiosController < ApplicationController
   def update
     @audio = Audio.find_by(public_uid: params[:id])
     @audio.assign_attributes(audios_params)
+    @audio.source = "glowing.mp3"
+    @audio.audio_image = 'noimage.png'
     @audio.user_id = current_user.id
     if @audio.save
       redirect_to "/",notice: "保存しました"

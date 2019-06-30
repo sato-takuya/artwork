3e19e35bbb49b0acfa8ef31f1af0cccac730f9a7
SATOTAKUYA <stakuya6@gmail.com>
2019-06-30 21:09:25 +0900

    s3up

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+    path_style: true
+  }
+
+  config.fog_public = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.remove_previously_stored_files_after_update = false
+  config.fog_directory  = 'art-wk'#バケット名
+  config.cache_storage = :fog
+
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+end

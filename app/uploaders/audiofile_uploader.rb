1fa8ce0847bd15085a880e89f0c10aad57c06dcb
SATOTAKUYA <stakuya6@gmail.com>
2019-07-01 11:34:59 +0900

    Test

--- app/uploaders/audiofile_uploader.rb
+++ app/uploaders/audiofile_uploader.rb
@@ -4,10 +4,12 @@ class AudiofileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production? || Rails.env.staging?
-    storage :fog
-  else
+  if Rails.env.development?
     storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
   end
 
   # Override the directory where uploaded files will be stored.

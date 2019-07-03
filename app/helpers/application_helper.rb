793c7004037c63113571ae78bae4617b9170d2c9
SATOTAKUYA <stakuya6@gmail.com>
2019-07-03 19:54:38 +0900

    Test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,28 @@
 module ApplicationHelper
+  def get_twitter_card_info(group,audio)
+    twitter_card = {}
+    if group.nil? && audio.nil?#トップページver
+      twitter_card[:url] = "https://www.art-wk.herokuapp.com/"
+      twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/art-wk/images/logo.png"
+      twitter_card[:description] = "バンド・ミュージシャンのためのポートフォリオサイト"
+      twitter_card[:title] = "artwork"
+    elsif group.present? && audio.nil?#グループver
+      twitter_card[:url] = "https://www.art-wk.herokuapp.com/#{group.nickname}"
+      twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/art-wk/images/logo.png"
+      twitter_card[:description] = group.name
+      twitter_card[:title] = "artwork"
+    else#オーディオver
+      twitter_card[:url] = "https://www.art-wk.herokuapp.com/#{audio.public_uid}"
+      twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/art-wk/images/logo.png"
+      twitter_card[:description] = audio.audio_title
+      twitter_card[:title] = "artwork"
+    end
+    twitter_card[:card] = 'summary_large_image'
+    twitter_card
+  end
+
+
+
   def movie(youtube_url)
     iframe = content_tag(
       :iframe,
